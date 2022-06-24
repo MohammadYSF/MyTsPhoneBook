f485fb63d62a6cb7d3bd1dd39ca89e066babd75c
Mohammad Yousefiyan <aa.yosefiyan7@gmail.com>
2022-06-25 00:59:03 +0430

    some improvements to the buttom component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,9 @@
 import { buttonType } from "../../types/button.types";
-const Button = (props:buttonType) => {
-    return(
-       <button className={props.bootstrapClasses.join(" ")}
-       onClick = {(e) => props.onClickHandler && props.onClickHandler(e)}
-       >{props.text}</button>
+const Button = (props: buttonType) => {
+    return (
+        <button className={props.bootstrapClasses.join(" ")}
+            onClick={(e) => props.onClickHandler && props.onClickHandler(e)}
+        >{props.text}</button>
 
     );
 }
