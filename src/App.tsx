e3cf4ea6946023fe423d7c7d1c26ac1a3df4fa8e
Mohammad Yousefiyan <aa.yosefiyan7@gmail.com>
2022-06-24 15:01:24 +0430

    builiding the  UI of App.tsx
    I imported bootstrap to the app.tsx file and i did some html and bootstrap classing . 
    not any component yet added to app.tsx!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-     
+      <div className='container'>
+        <div className='text-center'>
+          <h1 className='h1'>My ts phone book</h1>
+          <p>this is a simple phone book !</p>
+          <p>I have used these technologies to build this app : React , Typescript , Bootstrap</p>
+        </div>
+      </div>
     </div>
   );
 }
