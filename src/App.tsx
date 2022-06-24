52e6bdf528a731d0489087d25b9846a0e5a45dc3
Mohammad Yousefiyan <aa.yosefiyan7@gmail.com>
2022-06-25 01:00:55 +0430

    Adding the router feature to the project
    Also , I created a new component named Home and i just  transfered all of the code i had  been written to the Home component .
    And in App.js file , I imported the dependencies for routing in the react app and also i implemented them in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import People from './components/people';
-import Person from './components/person';
-import Button from './components/button';
+import NewContactForm from './components/newContactForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+
 function App() {
-  const deletePerson = (id:number):void =>{
-    console.log(`deleting person with id : ${id}`);
-  } 
-  const editPerson = (id:number):void =>{
-    console.log(`editting person with id : ${id}`);
-  } 
-  
   return (
-    <div className="App">
-      <div className='container'>
-        <div className='text-center my-5'>
-          <h1 className='h1'>My ts phone book</h1>
-          <p>this is a simple phone book !</p>
-          <p>I have used these technologies to build this app : React , Typescript , Bootstrap</p>
-          <Button text='add new !' bootstrapClasses={["btn" , "btn-success" , "px-5 py-1"]} type="button"/>
-        </div>
-        <div className='text-center'>
-          <People bootstrapTableClasses={["table","table-striped"]} >
-            <Person id={1} name="mohammad" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
-            <Person id={2} name="ali" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
-            <Person id={2} name="ehsan" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
-            <Person id={2} name="zolfian" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
-          </People>
-        </div>
-      </div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<NewContactForm />} path="/addNew" />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import People from '../people';
+import Person from '../person';
+import Button from '../button';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    const deletePerson = (id: number): void => {
+        console.log(`deleting person with id : ${id}`);
+    }
+    const editPerson = (id: number): void => {
+        console.log(`editting person with id : ${id}`);
+    }
+
+    return (
+        <div className="App">
+            <div className='container'>
+                <div className='text-center my-5'>
+                    <h1 className='h1'>My ts phone book</h1>
+                    <p>this is a simple phone book !</p>
+                    <p>I have used these technologies to build this app : React , Typescript , Bootstrap</p>
+            
+                <Link to={"/addNew"} className="btn btn-success px-5 py-1" type='button'>add new !</Link>
+                </div>
+                <div className='text-center'>
+                    <People bootstrapTableClasses={["table", "table-striped"]} >
+                        <Person id={1} name="mohammad" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+                        <Person id={2} name="ali" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+                        <Person id={2} name="ehsan" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+                        <Person id={2} name="zolfian" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+                    </People>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export default Home;
