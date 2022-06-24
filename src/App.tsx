4435d970fe13ddd50a44e0a0fea055b521384f0a
Mohammad Yousefiyan <aa.yosefiyan7@gmail.com>
2022-06-24 20:21:13 +0430

    using the person and people components in App.js
    I used people component and within it , I hard-coded some person component . Also , I created two functions "deletePerson" and "editPerson" which i will write their functionalities in near future

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import People from './components/people';
+import Person from './components/person';
 function App() {
+  const deletePerson = (id:number):void =>{
+    console.log(`deleting person with id : ${id}`);
+  } 
+  const editPerson = (id:number):void =>{
+    console.log(`editting person with id : ${id}`);
+  } 
+  
   return (
     <div className="App">
       <div className='container'>
@@ -8,6 +17,14 @@ function App() {
           <h1 className='h1'>My ts phone book</h1>
           <p>this is a simple phone book !</p>
           <p>I have used these technologies to build this app : React , Typescript , Bootstrap</p>
+        </div>
+        <div className='text-center'>
+          <People bootstrapTableClasses={["table","table-striped"]} >
+            <Person id={1} name="mohammad" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+            <Person id={2} name="ali" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+            <Person id={2} name="ehsan" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+            <Person id={2} name="zolfian" phoneNumber='09141257762' handleDelete={deletePerson} handleEdit={editPerson} />
+          </People>
         </div>
       </div>
     </div>
